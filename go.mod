dbf6800f5bda88f0e5a7aaf327345ad5e6fafa7f
Sumit Roy <sumit.roy@unacademy.com>
2020-01-30 18:11:25 +0530

    nitial commiR

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sroy8091/trie
+
+go 1.13
+
+require github.com/arpitbbhayani/tripod v0.0.0-20170425181942-66807adce3a5 // indirect
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+import "github.com/arpitbbhayani/tripod"
+
+func main() {
+ fmt.Println("Hello, World")
+ tripod.Init()
+}
